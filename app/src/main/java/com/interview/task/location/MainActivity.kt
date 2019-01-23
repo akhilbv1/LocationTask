573d16f0e01bed180e27d758c9884a73f60c2dcc
akhilbv1 <akhilbv1@gmail.com>
2019-01-23 18:26:55 +0530

    code has refractored

--- app/src/main/java/com/interview/task/location/MainActivity.kt
+++ app/src/main/java/com/interview/task/location/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.interview.task.location
 
 import android.Manifest
+import android.app.ProgressDialog
 import android.app.Service
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.support.annotation.RequiresApi
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
@@ -17,7 +20,7 @@ import android.widget.Toast
 import com.google.android.gms.location.*
 
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
     //list of permissions
     private val perms = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -26,19 +29,21 @@ class MainActivity : AppCompatActivity()  {
     private val PERMISSIONS_RESULT_CODE: Int = 125
 
     //whether the user switched on the gps or not
-    private var isNetwork: Boolean = false
     private var isGps: Boolean = false
 
     //location manager
     private lateinit var locationManager: LocationManager
 
-    private lateinit var tvLocation:TextView
+    private lateinit var tvLocation: TextView
 
     //location client to connect to google play services
-    private lateinit var mFusedLocationClient :FusedLocationProviderClient
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     //location callback
     private lateinit var mLocationCallback: LocationCallback
+
+    //progress Dialog
+    private lateinit var mloadingDialog:ProgressDialog
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +53,9 @@ class MainActivity : AppCompatActivity()  {
         val btnStart: Button = findViewById(R.id.btnStart)
         val btnStop: Button = findViewById(R.id.btnStop)
         tvLocation = findViewById(R.id.tvLocation)
+        mloadingDialog = ProgressDialog(this)
+        mloadingDialog.setMessage("Fetching Location...")
+
 
         locationManager = getSystemService(Service.LOCATION_SERVICE) as LocationManager
 
@@ -59,42 +67,66 @@ class MainActivity : AppCompatActivity()  {
         }
 
         btnStop.setOnClickListener {
-           // locationManager.removeUpdates(this)
+            // locationManager.removeUpdates(this)
             mFusedLocationClient.removeLocationUpdates(mLocationCallback)
         }
 
-        mLocationCallback = object :LocationCallback(){
+        mLocationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult?) {
                 super.onLocationResult(p0)
-              for (obj in p0?.locations!!)
-              {
-                  tvLocation.text = "Latitude:-${obj.latitude},Longitude:-${obj.longitude},Time:-${obj.time}"
+                if(mloadingDialog.isShowing){
+                    mloadingDialog.dismiss()
+                }
+                for (obj in p0?.locations!!) {
+                    tvLocation.text = "Latitude:-${obj.latitude},Longitude:-${obj.longitude},Time:-${obj.time}"
 
-              }
+                }
             }
         }
 
     }
 
     /**
+     * will return true if app has required permissions.
+     */
+    private fun hasRequiredPermissions(): Boolean = ContextCompat.checkSelfPermission(
+        applicationContext,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        application,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    /**
      * Will display the current loction to user in textview.
      */
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun getLatLng(){
-        isNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        isGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if(isNetwork && isGps)
-        {
-            try {
-            mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                tvLocation.text = "Latitude:-${location.latitude},Longitude:-${location.longitude},Time:-${location.time}"
-            }
-                setupPeriodicUpdates()
-            }catch (e:SecurityException)
-            {
-                checkAndRequestPermissions()
-            }
+    private fun getLatLng() {
+        if (hasRequiredPermissions()) {
+            isGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            if (isGps) {
+                try {
+                    mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                        if (location != null) {
+                            tvLocation.text =
+                                    "Latitude:-${location.latitude},Longitude:-${location.longitude},Time:-${location.time}"
+                        }
+                        else {
+                            tvLocation.text = ""
+                            mloadingDialog.show()
+                        }
+                    }
+                    setupPeriodicUpdates()
+                } catch (e: SecurityException) {
+                    checkAndRequestPermissions()
+                }
 
+            } else {
+                Toast.makeText(this@MainActivity,"Please switch on location and gps services for the app to work",Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            checkAndRequestPermissions()
         }
 
     }
@@ -104,7 +136,7 @@ class MainActivity : AppCompatActivity()  {
      * will setup periodic updates so that the location updates for every 5 seconds
      */
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun setupPeriodicUpdates(){
+    private fun setupPeriodicUpdates() {
         val mLocationRequest = LocationRequest()
         mLocationRequest.interval = 5000
         mLocationRequest.fastestInterval = 5000
@@ -117,8 +149,7 @@ class MainActivity : AppCompatActivity()  {
         task.addOnSuccessListener { locationSettingsResponse ->
             try {
                 mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null /* Looper */);
-            } catch (e:SecurityException)
-            {
+            } catch (e: SecurityException) {
                 checkAndRequestPermissions()
             }
         }
@@ -143,6 +174,7 @@ class MainActivity : AppCompatActivity()  {
         }
     }
 
+
     /**
      * if user rejects the permission than make a prompt and ask again
      */
@@ -152,7 +184,7 @@ class MainActivity : AppCompatActivity()  {
         when (requestCode) {
             PERMISSIONS_RESULT_CODE -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                   getLatLng()
+                    getLatLng()
                 } else {
                     requestPermissionsDialog()
                 }
